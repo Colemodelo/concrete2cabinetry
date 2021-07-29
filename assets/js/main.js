ff965f1b14a8c22cea61d6ac52ca8cf4000fd2f5
Cole Modelo <colemodelo@protonmail.com>
2021-07-28 21:07:25 -0600

    updated gallery slideshow

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,32 +65,34 @@
 
 })(jQuery);
 
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-} 
-
+document.querySelector(".prevBtn").addEventListener("click", () => {
+	changeSlides(-1);
+ });
+ document.querySelector(".nextBtn").addEventListener("click", () => {
+	changeSlides(1);
+ });
+ var slideIndex = 1;
+ showSlides(slideIndex);
+ function changeSlides(n) {
+	showSlides((slideIndex += n));
+ }
+ function currentSlide(n) {
+	showSlides((slideIndex = n));
+ }
+ function showSlides(n) {
+	var i;
+	var slides = document.getElementsByClassName("Slide");
+	var dots = document.getElementsByClassName("Navdot");
+	if (n > slides.length) {
+	   slideIndex = 1;
+	}
+	if (n < 1) {
+	   slideIndex = slides.length;
+	}
+	Array.from(slides).forEach(item => (item.style.display = "none"));
+	Array.from(dots).forEach(
+	   item => (item.className = item.className.replace(" selected", ""))
+	);
+	slides[slideIndex - 1].style.display = "block";
+	dots[slideIndex - 1].className += " selected";
+ }
